0de3b5185bbf00a9bfcc80978d4ed012526b2436
Kyle Carriedo <kcarriedo@gmail.com>
2019-07-31 16:59:26 -0400

    update event descriptor to have set properties.

--- Source/Timeline/EventDescriptor.swift
+++ Source/Timeline/EventDescriptor.swift
@@ -1,13 +1,13 @@
 import Foundation
 
-public protocol EventDescriptor {
-  var startDate: Date {get}
-  var endDate: Date {get}
-  var isAllDay: Bool {get}
-  var text: String {get}
-  var attributedText: NSAttributedString? {get}
-  var font : UIFont {get}
-  var color: UIColor {get}
-  var textColor: UIColor {get}
-  var backgroundColor: UIColor {get}
+public protocol EventDescriptor {  
+    var startDate: Date {get set}
+    var endDate: Date {get set}
+    var isAllDay: Bool {get set}
+    var text: String {get set}
+    var attributedText: NSAttributedString? {get set}
+    var font : UIFont {get set}
+    var color: UIColor {get set}
+    var textColor: UIColor {get set}
+    var backgroundColor: UIColor {get set}
 }
